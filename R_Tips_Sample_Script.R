08d675e93319b887400ce70357f4c31e006a3d68
Kate Schowe <kateschowe@MacBook-Air-4.local>
2022-09-24 13:58:52 -0500

    More tips added

--- R_Tips_Sample_Script.R
+++ R_Tips_Sample_Script.R
@@ -7,3 +7,10 @@
 
 ## When you want to look at the entire dataset, use:
     ## View(DataFrameName)
+
+## To check out a specific column while running through script, use:
+    ## unique(DataFrameName$ColumnName)
+
+## To save a file as a .csv, use:
+    ## write.csv(DataFrameName, file.path(main_dir,"AdditionalPathAfterMain",
+    ## paste0("FileName_", Sys.Date(), ".csv")),row.names = F)
