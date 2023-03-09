b25cd13e9ec9a280fe4a9e41dd38cf50c03965dd
Kate Good <you@example.com>
2023-03-09 10:48:31 -0600

    Added in Hernando's database (expert_comm)

--- 4-compile_occurrence_data_2023.R
+++ 4-compile_occurrence_data_2023.R
@@ -75,7 +75,7 @@ file_list <- list.files(file.path(main_dir,data,standard), pattern = ".csv",
                         full.names = T)
 file_dfs <- lapply(file_list, read.csv, header = T, na.strings = c("","NA"),
                    colClasses = "character")
-length(file_dfs) #20
+length(file_dfs) #21
 
 # stack all datasets using bind_rows, which keeps non-matching columns
 #   and fills with NA; 'Reduce' iterates through list and merges with previous.
@@ -85,10 +85,10 @@ nrow(all_data_raw) #348015
 names(all_data_raw) #37
 table(all_data_raw$database)
 # BIEN      CONABIO         CR          Ex_situ      Expert_Comm              FIA             GBIF 
-#  772        23307       1046             7260             2128              138            12492
+#  772        23320       1046             7260           10398              138            12492
 
 # iDigBio     IUCN_RedList NorthAm_herbaria              PMA             TEFH         Tropicos 
-# 4050           127759           191986              103              179              88
+# 4050           127759           191986              103              179              880
 
 # add unique identifier
 nms <- names(all_data_raw)
@@ -157,7 +157,7 @@ rm(matched,need_match,taxon_list_add,still_no_match)
 # keep only rows for target taxa
 all_data <- all_data %>% 
   filter(!is.na(taxon_name_status) & !is.na(UID))
-nrow(all_data) #30134
+nrow(all_data) #32226
 
 ### ! target taxa with no occurrence data:
 unique(taxon_list$taxon_name_acc)[
@@ -388,14 +388,11 @@ add_again$taxon_name_acc <- gsub(" var\\.*\\s.+", "",
 unique(add_again$taxon_name_acc)
 geo_pts <- rbind(geo_pts,add_again)
 table(geo_pts$database)
-#Base_Quercus          BIEN                Ex_situ                    FIA                   GBIF 
-#13                    708                    262                    138                   6880 
-#GT_USCG Herbario_TEFH_Honduras             iDigBio           IUCN_RedList               Maricela 
-#295                      2                   2215                   3611                    327 
-#NorthAm_herbaria         PMA    SAlvarez_Clare                   Sula        T_Blacks_Martin 
-#2248                     10                35                      9                     71
-#Tropicos
-#767
+#BIEN          CONABIO               CR          Ex_situ      Expert_Comm              FIA             GBIF 
+#708             1197              173              262             1638              138             6880
+
+#iDigBio     IUCN_RedList NorthAm_herbaria              PMA             TEFH         Tropicos
+# 2215             3611             2248               10                2              767
 
 # create rounded latitude and longitude columns for removing duplicates;
 #   number of digits can be changed based on how dense you want data
