adb87588371c14996313b8f7eb25c6f8b1906d61
Kate Good <you@example.com>
2023-08-14 14:29:20 -0500

    Added script if I needed to remove points

--- count_exsitu_institutions.R
+++ count_exsitu_institutions.R
@@ -11,15 +11,21 @@
         # 5) Percent of wild origin plants with known locality
 ###############################################################################
 
+#USE SCIPRT BELOW if you don't need to remove any points
+
+###############################################################################
+
 main_dir <- "/Volumes/GoogleDrive/My Drive/Franklinia/Mesoamerican Oak Gap Analysis 2023/occurrence_points/occurrence_data/georeferencing"
 
 # read in ex situ file
 ex_situ <- read.csv(file.path(main_dir,"ExSitu_Compiled_Post-Geolocation_2023-01-19.csv"), 
                     header = T, na.strings=c("","NA"),colClasses="character")
 
-# identify and count number of ex situ institutions with species 
 # update script with species name you are interested in 
-new <- ex_situ[which(ex_situ$taxon_name_accepted == "Quercus acherdophylla"),]
+new <- ex_situ[which(ex_situ$taxon_name_accepted == "Quercus acutifolia"),]
+
+
+# identify and count number of ex situ institutions with species 
 unique(new[c("inst_short")])
 nrow(unique(new[c("inst_short")]))
 
@@ -75,5 +81,78 @@ sum(L_only$num_indiv)
 X_only <- locality[which(locality$gps_det == "X"),]
 sum(X_only$num_indiv)
 
+###############################################################################
+
+#USE SCIPRT BELOW if you need to remove points based on UID before making table
+
+###############################################################################
+
+main_dir <- "/Volumes/GoogleDrive/My Drive/Franklinia/Mesoamerican Oak Gap Analysis 2023/occurrence_points/occurrence_data/georeferencing"
+
+# read in ex situ file
+ex_situ <- read.csv(file.path(main_dir,"ExSitu_Compiled_Post-Geolocation_2023-01-19.csv"), 
+                    header = T, na.strings=c("","NA"),colClasses="character")
+
+# update script with species name you are interested in 
+new <- ex_situ[which(ex_situ$taxon_name_accepted == "Quercus acutifolia"),]
+
+#new2 <- new[new$UID != "ArbBergerette~added0033~NG~Quercus cedrosensis", ]
+new2 <- new[!(new$UID %in% c("ChenesPlantesArb~LB97-0076~W~Quercus acutifolia", "ChenesPlantesArb~LB97-0075~W~Quercus acutifolia", "ChenesPlantesArb~LB97-0062~W~Quercus acutifolia", "ArbBergerette~added0071~NG~Quercus acutifolia","ArbBergerette~added0022~NG~Quercus acutifolia","ChevithorneBarton~added0068~NG~Quercus acutifolia","BGItirraran~added0070~NG~Quercus acutifolia")), ]
+
+# identify and count number of ex situ institutions with species 
+unique(new2[c("inst_short")])
+nrow(unique(new2[c("inst_short")]))
+
+# institution countries holding species. Re-names countries based on region and then 
+# count frequency of regions represented in collections
+#new$inst_country <-mgsub(new$inst_country,
+#c("United States","Spain","Honduras","Mexico","England","Belgium","France",
+#"Germany","Sweden","Australia","Canada","Scotland","NA","Argentina",
+#"Wales","Norway","Czech Republic","Netherlands","Finland","Denmark",
+#"Switzerland","South Korea","New Zealand","Israel","Poland","Georgia","Estonia"),
+#c("United States and Canada","Europe","Mesoamerica","Mesoamerica","Europe","Europe","Europe",
+#"Europe","Europe","Oceania","United States and Canada","Europe","NA","South America",
+#"Europe","Europe","Europe","Europe","Europe","Europe",
+#"Europe","Asia","Oceania","Asia","Europe","Europe","Europe"))
+Country <- unique(new2[c("inst_short","inst_country")])
+Country %>%
+  count(inst_country, name = "Country_Frequency")
+
+# count number of individuals
+new2$num_indiv <- as.numeric(new2$num_indiv)
+sum(new2$num_indiv)
+
+
+#average number of individuals per institution 
+avg <-aggregate(new2$num_indiv,list(new2$inst_short),sum)
+avg <- aggregate(num_indiv ~ inst_short, data = new2, sum)
+avg
+mean(avg$num_indiv)
+
+#if prov_type is NG or U but gps_det is G or L, change prov_type to W
+new2$prov_type <- ifelse((new2$prov_type == "NG" | new2$prov_type == "U") & (new2$gps_det == "G" | new2$gps_det == "L"), "W", new2$prov_type)
+
+
+# number of plants of wild origin 
+wild <-aggregate(new2$num_indiv,list(new2$prov_type),sum)
+wild <- aggregate(num_indiv ~ prov_type, data = new2, sum)
+wild
+
+
+# percent of wild origin plants with known locality
+# First, sort so only including prov type = W or Z
+locality <- new2[which(new2$prov_type == "W" | new2$prov_type == "Z"),]
+
+#number of wild origin plants recorded as G
+G_only <- locality[which(locality$gps_det == "G"),]
+sum(G_only$num_indiv)
+
+#number of wild origin plants recorded as L
+L_only <- locality[which(locality$gps_det == "L"),]
+sum(L_only$num_indiv)
+
+#number of wild origin plants recorded as X
+X_only <- locality[which(locality$gps_det == "X"),]
+sum(X_only$num_indiv)
 
 
